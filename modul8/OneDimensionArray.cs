ebe1f896002e9d81080cb8845d6b6b2571ac57ad
sadiev <sadiev@itstep.kz>
2019-04-16 19:00:46 +0600

    Добавьте файлы проекта.

---
+++ modul8/OneDimensionArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul8
+{
+  public class OneDimensionArray
+  {
+    private int[] numbers;
+    public OneDimensionArray(int size)
+    {
+      numbers = new int[size];
+    }
+    public int this[int index]
+    {
+      get { return numbers[index]; }
+      set { numbers[index] = value * value; }
+    }
+  }
+}
